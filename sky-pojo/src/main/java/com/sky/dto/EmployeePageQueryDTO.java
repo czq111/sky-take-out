1945f3296b2706701d9507e802b6ea811dc9ab02
czqq123456 <1807099124@qq.com>
2023-10-30 10:01:16 +0800

    员工分页查询

--- sky-pojo/src/main/java/com/sky/dto/EmployeePageQueryDTO.java
+++ sky-pojo/src/main/java/com/sky/dto/EmployeePageQueryDTO.java
@@ -5,15 +5,15 @@ import lombok.Data;
 import java.io.Serializable;
 
 @Data
-public class EmployeePageQueryDTO implements Serializable {
+    public class EmployeePageQueryDTO implements Serializable {
 
-    //员工姓名
-    private String name;
+        //员工姓名
+        private String name;
 
-    //页码
-    private int page;
+        //页码
+        private int page;
 
-    //每页显示记录数
-    private int pageSize;
+        //每页显示记录数
+        private int pageSize;
 
 }
